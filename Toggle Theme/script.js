4cd45190a4964d627766794486f7f42b5c4760c6
Rishab Mamgai <61201838+rishabmamgai@users.noreply.github.com>
2022-07-25 21:21:09 +0530

    With JavaScript

---
+++ Toggle Theme/script.js
@@ -0,0 +1,27 @@
+document.getElementById('black').addEventListener('click', function() {
+    document.getElementById('text').style.backgroundColor = 'black';
+    document.getElementsByTagName('textarea')[0].style.backgroundColor = 'black';
+    document.getElementsByTagName('textarea')[0].style.color = 'white';
+    document.getElementsByTagName('textarea')[0].style.borderColor = 'white';
+})
+
+document.getElementById('limegreen').addEventListener('click', function() {
+    document.getElementById('text').style.backgroundColor = 'limegreen';
+    document.getElementsByTagName('textarea')[0].style.backgroundColor = 'limegreen';
+    document.getElementsByTagName('textarea')[0].style.color = 'black';
+    document.getElementsByTagName('textarea')[0].style.borderColor = 'black';
+})
+
+document.getElementById('yellow').addEventListener('click', function() {
+    document.getElementById('text').style.backgroundColor = 'yellow';
+    document.getElementsByTagName('textarea')[0].style.backgroundColor = 'yellow';
+    document.getElementsByTagName('textarea')[0].style.color = 'black';
+    document.getElementsByTagName('textarea')[0].style.borderColor = 'black';
+})
+
+document.getElementById('deepskyblue').addEventListener('click', function() {
+    document.getElementById('text').style.backgroundColor = 'deepskyblue';
+    document.getElementsByTagName('textarea')[0].style.backgroundColor = 'deepskyblue';
+    document.getElementsByTagName('textarea')[0].style.color = 'white';
+    document.getElementsByTagName('textarea')[0].style.borderColor = 'white';
+})
